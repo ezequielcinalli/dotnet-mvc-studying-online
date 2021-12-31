69d1690568102f3eedf55e0e1f6d8049a43d2104
ezequiel <ezequielcinalli96@gmail.com>
2021-12-30 21:26:36 -0300

    Add register and login views

--- StudyingOnline/Models/User.cs
+++ StudyingOnline/Models/User.cs
@@ -8,10 +8,12 @@ public class User
 
     [Required]
     [StringLength(60, MinimumLength = 6, ErrorMessage = "min 6, max 60 letters")]
+    [EmailAddress]
     public string Email { get; set; } = String.Empty;
 
     [Required]
     [StringLength(60, MinimumLength = 3, ErrorMessage = "min 3, max 60 letters")]
+    [DataType(DataType.Password)]
     public string Password { get; set; } = String.Empty;
 
     [Required]
